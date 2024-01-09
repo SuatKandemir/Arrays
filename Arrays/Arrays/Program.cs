1839bb493cf7ca80aabab3abd64ec2c7541baa03
SuatKandemir <146870954+SuatKandemir@users.noreply.github.com>
2024-01-09 16:10:50 +0300

    Add

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Console.WriteLine("Please Enter The Number");
+int  arraylenght = int.Parse(Console.ReadLine());
+int[]  array = new int[arraylenght];
+
+for (int i = 0; i < arraylenght; i++)
+{
+    Console.WriteLine("Please {0}.Number Enter :",i+1);
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+int total = 0;
+foreach (int num in array)  
+   total += num;
+
+Console.WriteLine("Average :" + total / arraylenght);
